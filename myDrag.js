11e0c0c56c52b87819533db370c469116a61a170
Heinz <Heinz@Heinz-PC.fritz.box>
2014-11-03 11:32:57 +0100

    code to drag elements, in this case the dialog boxes

---
+++ myDrag.js
@@ -0,0 +1,58 @@
+function makeDrag(obj) {
+    'use strict';
+    var xy = {};
+
+    if (obj.style.position !== 'absolute') {
+        xy = absPos(obj);
+        obj.style.top = xy.y + 'px';
+        obj.style.left = xy.x + 'px';
+        obj.style.position = 'absolute';
+        obj.style.border = '1px solid black';// just for demo
+    }
+    obj.onmousedown = start;
+
+    function absPos(obj)
+    {// return absolute x,y position of obj
+        var ob, x = 0, y = 0;
+        x = obj.offsetLeft;
+        y = obj.offsetTop;
+        ob = obj.offsetParent;
+        while (ob !== null && ob.tagName !== 'BODY') {
+            x += ob.offsetLeft;
+            y += ob.offsetTop;
+            ob = ob.offsetParent;
+        }
+        return {'x': x, 'y': y};
+    }
+    function start(e)
+    {
+        if (e.ctrlKey) {
+            this.lastMouseX = e.clientX;
+            this.lastMouseY = e.clientY;
+            this.onmousemove = drag;
+            this.onmouseup = end;
+            this.style.cursor='move';
+            return false;
+        }
+    }
+    function drag(e)
+    {
+        var y = parseInt(this.style.top, 10),
+                x = parseInt(this.style.left, 10);
+
+        x = x + (e.clientX - this.lastMouseX);
+        y = y + (e.clientY - this.lastMouseY);
+
+        this.style.left = x + "px";
+        this.style.top = y + "px";
+        this.lastMouseX = e.clientX;
+        this.lastMouseY = e.clientY;
+        return false;
+    }
+    function end()
+    {
+        this.onmousemove = null;
+        this.onmouseup = null;
+         this.style.cursor='';
+    }
+}
