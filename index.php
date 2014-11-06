483b2fa9332a337d22538d3b8c11f6c4ea40afa2
Heinz <Heinz@Heinz-PC>
2014-11-06 15:13:53 +0100

    added dragHandle

--- index.php
+++ index.php
@@ -86,7 +86,7 @@ The latest version can allways be found at http://hgsweb.de
             </div>
             <p class="title">Alert , Confirmation , Prompt Dialog
                 <span class="small">All source code is included in this page</span>  </p>
-            <p>Use ctrl+leftMouseButton to drag dialog boxes.
+            <p>Move boxes using the striped area.
             <p id="out"><b></b></p> 
             <div style="margin-left:20px">
                 <button  onclick='myAlert("The Alert box\nYou made it !")'>Show Alert Box</button>
